8fd15e4707f0ca4202afcf08bc93a2cf8f41e395
Daniel Riverol <34141421+DanielRiverol@users.noreply.github.com>
2022-09-18 23:34:40 -0300

    Calculadora-v2

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const clock = document.querySelector(".clock");
+
+function reloj() {
+  let date = new Date();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
+  hours = (hours < 10) ? `0${hours}` : hours;
+  minutes = (minutes < 10) ? `0${minutes}` : minutes;
+  seconds = (seconds < 10) ? `0${seconds}` : seconds;
+
+  clock.innerText=`${hours}:${minutes}`
+}
+setInterval(reloj, 1000);
